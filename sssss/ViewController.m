8b438b3e3699c461297a35943d35aaffa553e4bf
JoyIos <646896350@qq.com>
2015-01-29 16:36:31 +0800

    init

--- sssss/ViewController.m
+++ sssss/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"-----------------");
+    for (int i = 0; i<100; i++) {
+        NSLog(@"%%%%%%%%%%%%%--------------%d",i);
+    }
 }
 
 - (void)didReceiveMemoryWarning {
