d95691afef11b00eaaa246b8dfdb632b3828a083
JoyIos <646896350@qq.com>
2015-01-29 16:32:03 +0800

    Initial Commit

---
+++ sssss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sssss
+//
+//  Created by QJ on 15/1/29.
+//  Copyright (c) 2015年 QJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sssss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sssss
+//
+//  Created by QJ on 15/1/29.
+//  Copyright (c) 2015年 QJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
